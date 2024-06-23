31b8cf8e00b23c304eb23467631fea8f4e06d1bc
Mohamed <moh.hossam.224@gmai.com>
2024-06-23 03:58:06 +0300

    ff

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,8 @@
-P = 1000
-R = 0.05
-T = 3
-I = P * R * T
-print ("The simple interest is:",I)
+number1 = 10 
+number2 = 5
+sum = number1 + number2
+Substarction = number1 - number2
+multiplication = number1 * number2
+print("sum of",number1,"and",number2,"is",sum)
+print("Subtraction",number1,"and",number2,"is",Substarction)
+print("Multiplication",number1,"and",number2,"is",multiplication)
